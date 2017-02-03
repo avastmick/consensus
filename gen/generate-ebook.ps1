392b29575023c6c1fb6be9b757d8a17a885770b5
avastmick <avastmick@outlook.com>
2017-02-03 15:32:16 +0800

    Renaming to allow use of Grammarly more easily

--- gen/generate-ebook.ps1
+++ gen/generate-ebook.ps1
@@ -1,6 +1,6 @@
 # To execute, need to run:
 # PowerShell.exe -ExecutionPolicy Unrestricted
 
-cat ..\chapters\*.md | sc ..\publish\draft.md
+cat ..\chapters\*.txt | sc ..\publish\draft.txt
 
-pandoc -f markdown -t epub --epub-cover-image=bookcover.jpg -o ..\publish\draft.epub --smart --toc ..\publish\draft.md
+pandoc -f markdown -t epub --epub-cover-image=bookcover.jpg --epub-stylesheet=epub.css -o ..\publish\draft.epub --smart --toc ..\publish\draft.txt
