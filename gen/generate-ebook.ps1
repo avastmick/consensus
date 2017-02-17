d304b5484c7d4a3ed69f58e74648a3cf94b192b3
avastmick <avastmick@outlook.com>
2017-02-17 23:48:17 +0800

    Refactor for gitbook cli test

--- gen/generate-ebook.ps1
+++
@@ -1,6 +0,0 @@
-# To execute, need to run:
-# PowerShell.exe -ExecutionPolicy Unrestricted
-
-cat ..\chapters\*.txt | sc ..\publish\draft.txt
-
-pandoc -f markdown -t epub --epub-cover-image=bookcover.jpg --epub-stylesheet=epub.css -o ..\publish\draft.epub --smart --toc ..\publish\draft.txt
