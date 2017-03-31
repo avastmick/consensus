2bf0f9309937cc9512dd2a1e0388644a4c4b528c
avastmick <avastmick@outlook.com>
2017-03-31 09:58:11 +0800

    Iteration over pandoc migration

--- build.sh
+++ build.sh
@@ -6,7 +6,16 @@
 # Container commands
 
 # Build a epub book
-# pandoc -S -o consensus-draft-$(date +"%m-%d-%y").epub chapters/*.md 
-# pandoc -S -o consensus-draft.epub chapters/*.md
+# pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub chapters/*.md 
+# pandoc -S -o consensus-draft.epub draft.md
 # Build a MOBI book
 # kindlegen *.epub
+
+
+
+# Brain tree photo
+# photo credit: Eduardo Mueses <a href="http://www.flickr.com/photos/38692570@N00/8650127689">Make the Connections</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
+
+
+# Cogs photo credit
+# photo credit: Piyushgiri Revagar <a href="http://www.flickr.com/photos/123868529@N03/28777007826">Clever Cogs!</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
