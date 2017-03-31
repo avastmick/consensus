191dd71c8d680fad48d72b204382329208b4ab9f
avastmick <avastmick@outlook.com>
2017-03-31 07:45:49 +0000

    Move to Pandoc as builder

--- build.sh
+++ build.sh
@@ -6,6 +6,12 @@
 # Container commands
 
 # Build a epub book
-# pandoc -S -o consensus-draft-$(date +"%m-%d-%y").epub chapters/*.md 
+# pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub pandoc.md chapters/*.md 
+# pandoc -S --toc-depth=1 -o consensus-draft.epub pandoc.md draft.md
 # Build a MOBI book
 # kindlegen *.epub
+
+
+
+# Cogs photo credit
+# photo credit: Piyushgiri Revagar <a href="http://www.flickr.com/photos/123868529@N03/28777007826">Clever Cogs!</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
---
+++ chapters/91-acknowledgements.md
@@ -0,0 +1,6 @@
+
+# Acknowledgements
+
+Cover art. Design by avastmick.
+
+Cover art image credit: Piyushgiri Revagar, http://www.flickr.com/photos/123868529@N03/28777007826 Clever Cogs! usage via a Creative Commons license.
