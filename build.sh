3dbfc05b4ec3c0c22eea8988d3002f7c695af136
avastmick <avastmick@outlook.com>
2017-04-01 16:41:48 +0800

    Fix stuffed merge on draft

--- build.sh
+++ build.sh
@@ -6,18 +6,7 @@
 # Container commands
 
 # Build a epub book
-<<<<<<< HEAD
-# pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub pandoc.md chapters/*.md 
-# pandoc -S --toc-depth=1 -o consensus-draft.epub pandoc.md draft.md
-# Build a MOBI book
-# kindlegen *.epub
 
-
-
-# Cogs photo credit
-# photo credit: Piyushgiri Revagar <a href="http://www.flickr.com/photos/123868529@N03/28777007826">Clever Cogs!</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
-=======
 pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub frontmatter.md chapters/*.md 
 # Build a MOBI book
 kindlegen *.epub
->>>>>>> edit
