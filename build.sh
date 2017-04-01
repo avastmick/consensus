d917a4b35936a6b4ebc4ca567fa81733feafee26
avastmick <avastmick@outlook.com>
2017-04-01 12:23:01 +0800

    Clean up build scripts - windows and linux

--- build.sh
+++ build.sh
@@ -1,17 +1,11 @@
 # Run the docker container (first time)
-# docker run -v C:\Users\avastmick\repos\consensus:/consensus --name consensus-box -i -t avastmick/ebook-builder:pandoc
+# docker run -v /home/avastmick/repos/consensus:/consensus:Z --name consensus-box -i -t avastmick/ebook-builder:pandoc
 # Start it
 # docker start -i consensus-box
 
 # Container commands
 
 # Build a epub book
-# pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub pandoc.md chapters/*.md 
-# pandoc -S --toc-depth=1 -o consensus-draft.epub pandoc.md draft.md
+pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub pandoc.md chapters/*.md 
 # Build a MOBI book
-# kindlegen *.epub
-
-
-
-# Cogs photo credit
-# photo credit: Piyushgiri Revagar <a href="http://www.flickr.com/photos/123868529@N03/28777007826">Clever Cogs!</a> via <a href="http://photopin.com">photopin</a> <a href="https://creativecommons.org/licenses/by-nc-nd/2.0/">(license)</a>
+kindlegen *.epub
