c33a54e9b4ec6448559b8cd9d58d1d10a729ae6b
avastmick <me@avastmick.org>
2017-04-18 07:33:59 +0800

    Created build script to generate various formats, including web publish

--- build.sh
+++ build.sh
@@ -5,26 +5,85 @@ WEBREPO="/home/avastmick/repos/avastmick.io"
 
 function usage () {
     echo $0: [function] [chapter count]
-    echo functions: word epub web
-    echo 'chapter count: number of chapters to include, or "all" for whole book'
+    echo '        function: word epub web'
+    echo '        chapter count: number of chapters to include, or "all" for whole book'
     exit 1
 }
 
 function word () {
-    echo 'Outputting '+$1+' chapter(s) of book in MS Word format'
+    local COUNT=0
+    CHAPTERS["$1"]=""
+    for file in chapters/*.md
+    do
+      if [[ ! -f "$file" ]]
+      then
+          continue
+      fi  
+      if [ "$1" = "all" ]; then
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      elif [ "$COUNT" -eq "$1" ]; then
+        break
+      else
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      fi                                                             
+    done
+    echo 'Outputting ' $1 ' chapter(s) of book in MS Word format'
+    pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").docx ${CHAPTERS[*]}
+    echo "...done. Written "${CHAPTERS[*]}" to docx file"
 }
 
 function publishToWeb () {
-    echo 'Publishing '+$1+' chapter(s) of book to website'
+    local COUNT=0
+    CHAPTERS["$1"]=""
+    for file in chapters/*.md
+    do
+      if [[ ! -f "$file" ]]
+      then
+          continue
+      fi 
+      if [ "$1" = "all" ]; then
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      elif [ "$COUNT" -eq "$1" ]; then
+        break
+      else
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      fi                                                             
+    done
 
-    local CHAPTERS="one"
+    echo 'Publishing ' $1 ' chapter(s) of book to website'
 
-    # cat $CHAPTERS > $WEBREPO/content/book/consensus/chapters.md
+    cat publish/frontmatter.md ${CHAPTERS[*]} publish/endmatter.md > $WEBREPO/content/book/consensus/chapters.md
+
+    echo "...done. Written "${CHAPTERS[*]}" to web site"
 }
 
 function epub () {
-    echo 'Outputting '+$1+' chapter(s) of book in epub'
-    pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub frontmatter.md chapters/*.md
+    local COUNT=0
+    CHAPTERS["$1"]=""
+    for file in chapters/*.md
+    do
+      if [[ ! -f "$file" ]]
+      then
+          continue
+      fi 
+      if [ "$1" = "all" ]; then
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      elif [ "$COUNT" -eq "$1" ]; then
+        break
+      else
+        CHAPTERS["$COUNT"]="$file"
+        let COUNT+=1
+      fi                                                            
+    done
+    echo 'Outputting ' $1 ' chapter(s) of book in epub'
+    pandoc -S --toc-depth=1 -o consensus-draft-$(date +"%m-%d-%y").epub frontmatter.md ${CHAPTERS[*]}
+
+    echo "...done. Written "${CHAPTERS[*]}" to epub file"
 }
 
 if [ $# -ne 2 ]; then
@@ -34,5 +93,7 @@ elif [ $1 == 'web' ]; then
 elif [ $1 == 'epub' ]; then
     epub $2
 elif [ $1 == 'word' ]; then
-        #statements
+    word $2
+elif [[ condition ]]; then
+    usage
 fi
